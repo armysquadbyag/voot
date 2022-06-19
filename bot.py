edd68235e606ee5840d80d2184e612e824f5c70f
Kenil Patel <68728138+kenil261615@users.noreply.github.com>
2022-06-19 10:55:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bot = Client('stevebot',
 
 str_session= "AQDotA4AcdCLvWArKwYkHtN8Qv_g4eTYAx2nzZ5ecZUqFneRppewP5W1sIw0F0w_SNx0vqoe_oRraKEg5bz87ANzyUJ2RY5nZOpoA90-qzF02HJkaaZdBEIqGKDmU4PY5D3f5Jy8NR6kPPMXHh_h4_-rK4ReNDBqrFXtNFfVUfmxO5w7LMCKLvpUeBuhwY3JXDDJ86rYyJLb4W-BNVUriK6TcvGKerjzQ0I-VK10466cqEzisXmgKt_HjHqwx__XnfMW4gGPGqTGoJdXgv3S_I56MqKPiqh-RJ3AQVH_JFjib6kfRT2bB_MDO4tfoRZGhj8-sGBPy8fFV2C98SwL42ZCPLBbAQAAAAEwvvUkAA"
 
-app = Client(name='vootmirror', api_id=15250446, api_hash="fc4fc6f48b69461dae88f60f300ce19e", session_string=str_session)
+app = Client(name='vootmirrorr', api_id=15250446, api_hash="fc4fc6f48b69461dae88f60f300ce19e", session_string=str_session)
 
 mirr_cmd = "/mirror"
 mirr_chat = "-1001677317467"
@@ -64,5 +64,5 @@ async def id(bot, message):
             del_msg_id = (int(error_msg.id) - 1)
             await bot.delete_messages(message.chat.id, del_msg_id)
 print("Bot Started!")
-bot.run()
 app.run()
+bot.run()
